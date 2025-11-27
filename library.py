ec632197af8844acc3f3429c03e9a907852d6c39
msdahsan <143450633+xelev@users.noreply.github.com>
2025-11-27 13:53:40 -0500

    First version of library
    First version of the project. This version allows users to create profiles with validation to make sure their DOB and name use correct syntax.Users are given an id number that they can use to log into their profile and check out book. Checking out books works good by adding books to the user's profile and preventing them from checking out books if they have more than 3 books checked out or if they owe money. Future steps: instead of using 2 parallel lists for the ID number and the user object, use a dictionary to connect the two, use a dictionary when checking out books to connect the book checked out with the date it was checked out, implement other features to allow for tracking how long books have been checked out and concurring fees on user's account based on that.

---
+++ library.py
@@ -0,0 +1,199 @@
+import random
+import string
+import datetime
+
+class User:
+    
+    id_list = []
+    user_list = []
+    total_library_users = 0
+    total_money_owed = 0
+    total_books_passed_due = 0
+    
+    def __init__(self, books_checked_out, books_passed_return, money_owed,
+                 id, name, birthday):
+        
+        self._books_checked_out = books_checked_out
+        self._books_passed_return = books_passed_return
+        self._money_owed = money_owed
+
+        id = random.randint(10000000, 99999999)
+        while (id in self.id_list):
+            id = random.randint(10000000, 99999999)
+        self._id = str(id)
+
+        name_status = False
+        letter_status = False
+        while (name_status == False):
+            for i in name:
+                if (i in string.punctuation or i.isdigit()):
+                    letter_status = False
+                    print("Error, you cannot have punctuation or numbers in your name")
+                    name = input("Please enter your full name: ")
+                    break
+                else:
+                    letter_status = True
+            if (letter_status == True):
+                name_status = True
+            else:
+                name_status = False
+        self._name = name
+
+        birthday_status = False
+        while (birthday_status == False):
+            try: 
+                birthday = datetime.datetime.strptime(birthday, "%m/%d/%Y")
+                birthday_status = True
+            except:
+                print("Incorrect date format, please try again!: ")
+                birthday = input("Please enter your birthday in the following format: DD/MM/YYYY ")
+        self._birthday = birthday
+
+    @property
+    def books_checked_out(self):
+        return self._books_checked_out
+
+    @property
+    def books_passed_return(self):
+        return self._books_passed_return
+    
+    @property
+    def money_owed(self):
+        return self._money_owed
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @property 
+    def name(self):
+        return self._name
+    
+    @property
+    def birthday(self):
+        return self._birthday
+    
+    @books_checked_out.setter
+    def books_checked_out(self, change_in_books):
+        self._books_checked_out += change_in_books
+
+    @books_passed_return.setter
+    def books_passed_return(self, change_in_passed_books):
+        self._books_passed_return += change_in_passed_books
+
+    @money_owed.setter
+    def money_owed(self, change_in_money):
+        self._money_owed += change_in_money
+
+    @name.setter
+    def name(self, new_name):
+        name_status = False
+        letter_status = False
+        while (name_status == False):
+            for i in new_name:
+                if (i in string.punctuation or i.isDigit()):
+                    letter_status = False
+                    print("Error, you cannot have punctuation or numbers in your name")
+                    new_name = input("Enter your name: ")
+                    break
+                else:
+                    letter_status = True
+            if (letter_status == True):
+                name_status = True
+            else:
+                name_status = False
+        self._name = new_name
+
+    @birthday.setter
+    def birthday(self, new_birthday):
+        birthday_status = False
+        while (birthday_status == False):
+            try: 
+                new_birthday = datetime.datetime.strptime(new_birthday, "%d/%m/%Y")
+                birthday_status = True
+            except:
+                print("Incorrect date format, please try again!: ")
+        self._birthday = new_birthday
+
+class Book:
+
+    books_list = []
+    available_books = [] 
+
+    def __init__(self, name, author, release_date, language, genre, age_class, price,
+                 is_checked_out):
+        
+        self._name = name
+        self._author = author
+        self._release_date = release_date
+        self._language = language
+        self._genre = genre
+        self._age_class = age_class
+        self._price = price
+        self._is_checked_out = is_checked_out
+
+
+    @property
+    def name(self):
+        return self._name
+        
+    @property
+    def author(self):
+        return self._author
+        
+    @property
+    def release_date(self):
+        return self._release_date
+        
+    @property
+    def language(self):
+        return self._language
+    
+    @property
+    def genre(self):
+        return self._genre
+    
+    @property
+    def age_class(self):
+        return self._age_class
+    
+    @property
+    def price(self):
+        return self._price
+    
+    @property
+    def is_checked_out(self):
+        return self._is_checked_out
+    
+    @name.setter
+    def name(self, name_to_add):
+        self._name = name_to_add
+    
+    @author.setter
+    def author(self, author_to_add):
+        self._author = author_to_add
+
+    @release_date.setter
+    def release_date(self, added_release_date):
+        self._release_date = added_release_date
+
+    @language.setter
+    def language(self, language_to_add):
+        self._language = language_to_add
+
+    @genre.setter
+    def genre(self, genre_to_add):
+        self._genre = genre_to_add
+
+    @age_class.setter
+    def age_class(self, age_class_to_add):
+        self._age_class = age_class_to_add
+
+    @price.setter
+    def price(self, price_to_add):
+        self._price = price_to_add
+
+    @is_checked_out.setter
+    def is_checked_out(self, status):
+        self._is_checked_out = status
+    
---
+++ library_main.py
@@ -0,0 +1,121 @@
+import csv
+import time
+import library
+
+#Get user object from ID
+def get_user_object(id_number):
+    user_index = library.User.id_list.index(id_number)
+    current_user = library.User.user_list[user_index]
+    return current_user
+
+#Create function to validate if user can check out books
+def validate_check_out(id_number):
+    current_user = get_user_object(id_number)
+    if (len(current_user.books_checked_out) >= 3):
+        print("You are not allowed to check this book out because you already have 3 books checked_out")
+        return False
+    elif (current_user.money_owed > 0):
+        print("You are not allowed to check this book out because you have an outstanding balance on yoru account")
+        return False
+    else:
+        return True
+
+#Create function to check out book from library
+def check_out_book(user, book_index):
+    user.books_checked_out.append(library.Book.books_list)
+    pass
+
+#Create function to represent loading time
+def loading_time():
+    print("Loading ", end="", flush=True)
+    for i in range(3):
+        time.sleep(1)
+        print(". ", end="", flush=True)
+    print("\n", flush=True)
+
+
+#Load in libary books
+with open("booksdata.csv", "r") as book_in:
+    book_data = csv.reader(book_in)
+    for row in book_data:
+        book_name = row[0]
+        if book_name == "Name":
+            continue
+        book_author = row[1]
+        book_release_date = row[2]
+        book_language = row[3]
+        book_genre = row[4]
+        book_age_class = row[5]
+        book_price = row[6]
+        book_is_checked_out = False
+        book = library.Book(book_name, book_author, book_release_date, book_language, 
+                            book_genre, book_age_class, book_price, book_is_checked_out)
+        library.Book.books_list.append(book)
+
+#Fill in list of books avaialble to be checked out
+for book in library.Book.books_list:
+    library.Book.available_books.append(book)
+
+#Print main menu
+def main_menu():
+    print()
+    print("Main Menu \n")
+    print("Enter the number that corresponds with what you would like to do")
+    print("------------------------------------------------------- \n")
+    print("1. View all of the library's books")
+    print("2. View all books available for check out (requires sign in)")
+    print("3. View profile information")
+    print("4. Create a user profile \n")
+    print("5. Exit the program")
+    print("-------------------------------------------------------")
+
+
+#Run program
+print("Welcome to Maad's Library! \n")
+main_menu()
+user_input = input()
+while (user_input != 5):
+
+    if (user_input == '1'):
+        loading_time()
+        for book in library.Book.books_list:
+            print(book.name)
+
+    elif (user_input == '2'):
+        id_input = input("Please enter your ID number to validate your profile: ")
+        if (id_input not in library.User.id_list):
+            print("That is not a valid id in our system")
+        else:
+            loading_time()
+            increment = 1
+            for book in library.Book.available_books:
+                    print(f"{increment}. {book.name}")
+                    increment += 1
+            book_num_input = input("Enter the number corresponding with the book you want to check out \n" \
+            "If you want to return to the main menu, type 'x' ")
+            print("")
+            if (book_num_input.lower() == 'x'):
+                continue
+            else:
+                book_num_input = int(book_num_input)
+                is_allowed = validate_check_out(id_input)
+                if (is_allowed):
+                    current_user = get_user_object(id_input)
+                    current_user.books_checked_out.append(library.Book.available_books[book_num_input-1])
+                    print(f"You successfully checked out {library.Book.available_books[book_num_input-1].name}")
+                    library.Book.available_books.pop(book_num_input-1)
+                    library.Book.books_list[book_num_input-1].is_checked_out = True
+            
+    elif (user_input == '4'):
+        print("Thank you for choosing to become a user")
+        entered_name = input("Please enter your full name: ")
+        enetered_birthday = input("Please enter your birthday in the following format: DD/MM/YYYY ")
+        user = library.User(books_checked_out=[], books_passed_return=0, money_owed=0, id=None,
+                            name=entered_name, birthday=enetered_birthday)
+        library.User.id_list.append(user.id)
+        library.User.user_list.append(user)
+        print(f"You have now been registered as a user and your unique id is {user.id}")
+    
+    main_menu()
+    user_input = input()
+    
